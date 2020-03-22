e214ff8fb67a6768f9fc94a8433b1b57d3023267
Vinicius Cassales - Level4 <viniciuscassales@hotmail.com>
2020-03-22 15:47:35 -0300

    Refatorado para criar um super-controller com funções básicas de crud

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,62 +1,11 @@
 "use strict";
 
 const mongoose = require('mongoose')
-const Product = mongoose.model('Product')
-const repository = require('../repositories/product-repository')
+const Product = mongoose.model('Product');
 
-exports.create = async(req, res) => {
-    try {
-        const product = await repository.create(req.body);
-        req.body._id = product._id;
-        res.status(201).send(req.body);
-    } catch (error) {
-        res.status(400).send({
-            message: "Falha ao cadastrar produto"
-            ,data: e
-        });
-    }
-};
+const controller = require("./super-controller");
+const repository = require('../repositories/product-repository');
+controller.setRepository(repository);
+controller.setEntity("Produto");
 
-exports.list = async(req, res) => {
-    var data = await repository.list();
-    res.status(200).send(data);
-};
-
-exports.get = async(req, res) => {
-    try {
-        const product = await repository.get(req.params);
-        res.status(200).send(product);
-    } catch (error) {
-        res.status(500).send({
-            message: "Falha ao obter produto"
-            ,data: error
-        });
-    }
-};
-
-exports.update = async(req, res) => {
-    try {
-        await repository.update(req.params, req.body);
-        res.status(200).send(req.body);
-    } catch (error) {
-        res.status(500).send({
-            message: "Falha ao atualizar produto"
-            ,data: e
-        });
-    }
-    
-};
-
-exports.delete = async(req, res) => {
-    try {
-        await repository.delete(req.params.id);
-        res.status(200).send({
-            messagem : "Produto Removido com sucesso"
-        });
-    } catch (error) {
-        res.status(400).send({
-            message: "Falha ao remover produto"
-            ,data: e
-        });
-    }
-};
+module.exports = controller;
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -1,10 +1,15 @@
 "use strict";
 
-/* Define as rotas básicas de CRUD
-@TODO criar um novo parâmetro para poder dizer quais rotas quer
-*/
 const superRoute = require("./super-route");
 var controllerName = 'product';
-var router = superRoute.init(controllerName)
+const definedRoutes = {
+    get: true,
+    getOne: true,
+    create: true,
+    update: true,
+    delete: true,
+};
+
+var router = superRoute.init(controllerName, definedRoutes)
 
 module.exports = router;
